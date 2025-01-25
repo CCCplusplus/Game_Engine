8bafb9ba3773a8630808622cbef9f24c04a81614
CCCplusplus <139177589+CCCplusplus@users.noreply.github.com>
2025-01-25 05:03:55 -0600

    Class and jumping cube

--- src/main.c
+++ src/main.c
@@ -1,30 +1,204 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
-
+#include "rlgl.h"
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include <stdio.h>
 
-int main ()
+
+void DrawCubeTexture(Texture2D texture, Vector3 position, float width, float height, float length, Color color)
+{
+    float x = position.x;
+    float y = position.y;
+    float z = position.z;
+
+    // Set desired texture to be enabled while drawing following vertex data
+    rlSetTexture(texture.id);
+
+    // Vertex data transformation can be defined with the commented lines,
+    // but in this example we calculate the transformed vertex data directly when calling rlVertex3f()
+    //rlPushMatrix();
+        // NOTE: Transformation is applied in inverse order (scale -> rotate -> translate)
+        //rlTranslatef(2.0f, 0.0f, 0.0f);
+        //rlRotatef(45, 0, 1, 0);
+        //rlScalef(2.0f, 2.0f, 2.0f);
+
+    rlBegin(RL_QUADS);
+    rlColor4ub(color.r, color.g, color.b, color.a);
+    // Front Face
+    rlNormal3f(0.0f, 0.0f, 1.0f);       // Normal Pointing Towards Viewer
+    rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width / 2, y - height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+    rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width / 2, y - height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+    rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width / 2, y + height / 2, z + length / 2);  // Top Right Of The Texture and Quad
+    rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x - width / 2, y + height / 2, z + length / 2);  // Top Left Of The Texture and Quad
+    // Back Face
+    rlNormal3f(0.0f, 0.0f, -1.0f);     // Normal Pointing Away From Viewer
+    rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x - width / 2, y - height / 2, z - length / 2);  // Bottom Right Of The Texture and Quad
+    rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x - width / 2, y + height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+    rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x + width / 2, y + height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+    rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x + width / 2, y - height / 2, z - length / 2);  // Bottom Left Of The Texture and Quad
+    // Top Face
+    rlNormal3f(0.0f, 1.0f, 0.0f);       // Normal Pointing Up
+    rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x - width / 2, y + height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+    rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width / 2, y + height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+    rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width / 2, y + height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+    rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width / 2, y + height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+    // Bottom Face
+    rlNormal3f(0.0f, -1.0f, 0.0f);     // Normal Pointing Down
+    rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x - width / 2, y - height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+    rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x + width / 2, y - height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+    rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x + width / 2, y - height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+    rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x - width / 2, y - height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+    // Right face
+    rlNormal3f(1.0f, 0.0f, 0.0f);       // Normal Pointing Right
+    rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x + width / 2, y - height / 2, z - length / 2);  // Bottom Right Of The Texture and Quad
+    rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x + width / 2, y + height / 2, z - length / 2);  // Top Right Of The Texture and Quad
+    rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x + width / 2, y + height / 2, z + length / 2);  // Top Left Of The Texture and Quad
+    rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x + width / 2, y - height / 2, z + length / 2);  // Bottom Left Of The Texture and Quad
+    // Left Face
+    rlNormal3f(-1.0f, 0.0f, 0.0f);    // Normal Pointing Left
+    rlTexCoord2f(0.0f, 0.0f); rlVertex3f(x - width / 2, y - height / 2, z - length / 2);  // Bottom Left Of The Texture and Quad
+    rlTexCoord2f(1.0f, 0.0f); rlVertex3f(x - width / 2, y - height / 2, z + length / 2);  // Bottom Right Of The Texture and Quad
+    rlTexCoord2f(1.0f, 1.0f); rlVertex3f(x - width / 2, y + height / 2, z + length / 2);  // Top Right Of The Texture and Quad
+    rlTexCoord2f(0.0f, 1.0f); rlVertex3f(x - width / 2, y + height / 2, z - length / 2);  // Top Left Of The Texture and Quad
+    rlEnd();
+    //rlPopMatrix();
+
+    rlSetTexture(0);
+}
+
+// Draw cube with texture piece applied to all faces
+void DrawCubeTextureRec(Texture2D texture, Rectangle source, Vector3 position, float width, float height, float length, Color color)
+{
+    float x = position.x;
+    float y = position.y;
+    float z = position.z;
+    float texWidth = (float)texture.width;
+    float texHeight = (float)texture.height;
+
+    // Set desired texture to be enabled while drawing following vertex data
+    rlSetTexture(texture.id);
+
+    // We calculate the normalized texture coordinates for the desired texture-source-rectangle
+    // It means converting from (tex.width, tex.height) coordinates to [0.0f, 1.0f] equivalent 
+    rlBegin(RL_QUADS);
+    rlColor4ub(color.r, color.g, color.b, color.a);
+
+    // Front face
+    rlNormal3f(0.0f, 0.0f, 1.0f);
+    rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x - width / 2, y - height / 2, z + length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x + width / 2, y - height / 2, z + length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+    rlVertex3f(x + width / 2, y + height / 2, z + length / 2);
+    rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+    rlVertex3f(x - width / 2, y + height / 2, z + length / 2);
+
+    // Back face
+    rlNormal3f(0.0f, 0.0f, -1.0f);
+    rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x - width / 2, y - height / 2, z - length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+    rlVertex3f(x - width / 2, y + height / 2, z - length / 2);
+    rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+    rlVertex3f(x + width / 2, y + height / 2, z - length / 2);
+    rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x + width / 2, y - height / 2, z - length / 2);
+
+    // Top face
+    rlNormal3f(0.0f, 1.0f, 0.0f);
+    rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+    rlVertex3f(x - width / 2, y + height / 2, z - length / 2);
+    rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x - width / 2, y + height / 2, z + length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x + width / 2, y + height / 2, z + length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+    rlVertex3f(x + width / 2, y + height / 2, z - length / 2);
+
+    // Bottom face
+    rlNormal3f(0.0f, -1.0f, 0.0f);
+    rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+    rlVertex3f(x - width / 2, y - height / 2, z - length / 2);
+    rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+    rlVertex3f(x + width / 2, y - height / 2, z - length / 2);
+    rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x + width / 2, y - height / 2, z + length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x - width / 2, y - height / 2, z + length / 2);
+
+    // Right face
+    rlNormal3f(1.0f, 0.0f, 0.0f);
+    rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x + width / 2, y - height / 2, z - length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+    rlVertex3f(x + width / 2, y + height / 2, z - length / 2);
+    rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+    rlVertex3f(x + width / 2, y + height / 2, z + length / 2);
+    rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x + width / 2, y - height / 2, z + length / 2);
+
+    // Left face
+    rlNormal3f(-1.0f, 0.0f, 0.0f);
+    rlTexCoord2f(source.x / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x - width / 2, y - height / 2, z - length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, (source.y + source.height) / texHeight);
+    rlVertex3f(x - width / 2, y - height / 2, z + length / 2);
+    rlTexCoord2f((source.x + source.width) / texWidth, source.y / texHeight);
+    rlVertex3f(x - width / 2, y + height / 2, z + length / 2);
+    rlTexCoord2f(source.x / texWidth, source.y / texHeight);
+    rlVertex3f(x - width / 2, y + height / 2, z - length / 2);
+
+    rlEnd();
+
+    rlSetTexture(0);
+}
+
+int main(int argc, char** argv)
 {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
+    int resX = 1280;
+    int resY = 800;
+    bool wantsFullscreen = false;
+    if (argc > 1)
+    {
+        for (int i = 0; i < argc; i++)
+        { 
+            //std::cout << "arg " << i << argv[i] << std::endl;
+            fprintf(stderr, "arg %i : %s \n", i, argv[i]);
+            if (strcmp(argv[i], "-resx") == 0) 
+            {
+                resX = atoi(argv[i + 1]);
+            }
+            if (strcmp(argv[i], "-resy") == 0)
+            {
+                resY = atoi(argv[i + 1]);
+            }
+            if (strcmp(argv[i], "-fullscreen") == 0)
+            {
+                //wantsFullscreen = true;
+            }
+        }
+    }
+
 	// Create the window and OpenGL context
 	InitWindow(1280, 800, "Hello Raylib");
-	// ToggleFullscreen();
+	if (wantsFullscreen) ToggleFullscreen();
+
+
+    Vector3 position = { 0.0f, 0.0f, 0.0f };                    // Set model position
+
+    //BoundingBox bounds = GetMeshBoundingBox(model.meshes[0]);   // Set model bounds
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
+
+	Texture cubetext = LoadTexture("wood.png");
+
 
 	Camera3D camera = { 0 };
 	camera.position = (Vector3){ 4, 0, 2 };
@@ -33,10 +207,19 @@ int main ()
 	camera.fovy = 45;
 	camera.projection = CAMERA_PERSPECTIVE;
 
+	float cubeX = 0;
+	float cubeY = 0;
+	float cubeZ = 0;
+
+	float gravity = 0.5;
+
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 		UpdateCamera(&camera, CAMERA_FREE);
+
+
+
 		// drawing
 		BeginDrawing();
 
@@ -49,9 +232,27 @@ int main ()
 		// draw our texture to the screen
 		//DrawTexture(wabbit, 400, 200, WHITE);
 
+		
+
 		BeginMode3D(camera);
 
-		DrawCube((Vector3) { 0, 0, 0 }, 5,5,5, RED);
+
+
+		//DrawCube((Vector3) { cubeX, cubeY, cubeZ }, 5,5,5, RED);
+        DrawCubeTexture(cubetext, (Vector3) { cubeX, cubeY, cubeZ }, 5, 5, 5, RAYWHITE);
+
+		if (cubeY != 0.0)
+			cubeY -= gravity;
+		
+		DrawGrid(20, 10);
+
+		if (IsKeyDown(KEY_W)) cubeX+= 0.5;
+		if (IsKeyDown(KEY_S)) cubeX -= 0.5;
+		if (IsKeyDown(KEY_A)) cubeZ -= 0.5;
+		if (IsKeyDown(KEY_D)) cubeZ += 0.5;
+
+		if(cubeY != 10)
+		if (IsKeyPressed(KEY_SPACE)) cubeY += 15;
 
 		EndMode3D();
 
@@ -64,6 +265,7 @@ int main ()
 	// cleanup
 	// unload our texture so it can be cleaned up
 	UnloadTexture(wabbit);
+
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
